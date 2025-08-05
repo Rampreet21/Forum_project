ec6244a316dcf4f0586d9b6ab96ede22f908adf7
Rampreet21 <rr6975871@gmail.com>
2025-08-05 22:13:42 +0530

    me

--- security/DB_connect.php
+++ security/DB_connect.php
@@ -1,13 +1,13 @@
 <?php
 
-$server = "db.us-losa1.bengt.wasmernet.com";
-$user = "22e3c590723c80001159b33c1d21";
-$password = "068922e3-c590-7563-8000-80213cd4c006";
-// $database = "dbmK3ksj9PzcWsBTpcFQUqEy";
+// $server = "db.us-losa1.bengt.wasmernet.com";
+// $user = "22e3c590723c80001159b33c1d21";
+// $password = "068922e3-c590-7563-8000-80213cd4c006";
+// // $database = "dbmK3ksj9PzcWsBTpcFQUqEy";
 
-$conn = mysqli_connect($server, $user, $password);
+// $conn = mysqli_connect($server, $user, $password);
 
-if(!$conn) {
-    die("Commection Error". mysqli_connect_error());
-}
+// if(!$conn) {
+//     die("Commection Error". mysqli_connect_error());
+// }
 ?>
