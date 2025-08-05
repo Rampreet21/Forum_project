c00925e22af25a304acd3f764102681bf0e06612
Rampreet21 <rr6975871@gmail.com>
2025-08-05 21:49:56 +0530

    me

--- security/DB_connect.php
+++ security/DB_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "x_forum";
+$server = "db.us-losa1.bengt.wasmernet.com";
+$user = "22e3c590723c80001159b33c1d21";
+$password = "068922e3-c590-7563-8000-80213cd4c006";
+$database = "dbmK3ksj9PzcWsBTpcFQUqEy";
 
 $conn = mysqli_connect($server, $user, $password, $database);
 
