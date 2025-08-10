038904116335cd8be0f268d15b9385a08e4069b7
Rampreet Kumar <rkofficial1st@gmail.com>
2025-08-10 23:27:03 +0530

    Update DB_connect.php

--- security/DB_connect.php
+++ security/DB_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-// $server = "db.us-losa1.bengt.wasmernet.com";
-// $user = "22e3c590723c80001159b33c1d21";
-// $password = "068922e3-c590-7563-8000-80213cd4c006";
-// // $database = "dbmK3ksj9PzcWsBTpcFQUqEy";
+// $server = "";
+// $user = "";
+// $password = "";
+// // $database = "";
 
 // $conn = mysqli_connect($server, $user, $password);
 
